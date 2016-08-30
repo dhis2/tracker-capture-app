dad142e7d49944150fefe0c28afebdd14fdf24a0
abyot <abyota@gmail.com>
2016-08-30 09:52:56 +0200

    bug fix in generating TEI reprts

--- components/report/tei-report-controller.js
+++ components/report/tei-report-controller.js
@@ -125,7 +125,6 @@ trackerCapture.controller('TeiReportController',
         $scope.stagesById = [];  
         $scope.allowProvidedElsewhereExists = [];
         $scope.prStDes = [];
-        $scope.dataElementTranslations = CurrentSelection.getDataElementTranslations();
         
         ProgramStageFactory.getByProgram($scope.selectedProgram).then(function(stages){
             $scope.programStages = stages;
@@ -136,8 +135,7 @@ trackerCapture.controller('TeiReportController',
                         providedElsewhereExists = true;
                         $scope.allowProvidedElsewhereExists[stage.id] = true;
                     }
-                    var tx = $scope.dataElementTranslations[stage.programStageDataElements[i].dataElement.id];
-                    stage.programStageDataElements[i].dataElement.displayFormName = tx.displayFormName && tx.displayFormName !== "" ? tx.displayFormName : tx.displayName ? tx.displayName : stage.programStageDataElements[i].dataElement.displayName;
+                    
                     $scope.prStDes[stage.programStageDataElements[i].dataElement.id] = stage.programStageDataElements[i];
                 }
 
