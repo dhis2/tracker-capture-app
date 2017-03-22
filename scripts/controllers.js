e9f09b5ca8f1d84e2434c4695ae2210421da23c9
Jiju K Jose <jijukjose@gmail.com>
2017-03-22 14:50:30 +0100

    R d8dcc50

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -115,6 +115,7 @@ function($rootScope,
                     if(orgUnitFromStore) {
                         selections.orgUnit = orgUnitFromStore;
                         CurrentSelection.set(selections);
+                        $scope.selectedOrgUnit.closedStatus = orgUnitFromStore.closedStatus;
                     }
                 });
             }
