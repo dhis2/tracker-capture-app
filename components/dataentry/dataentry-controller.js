02d1466c4d7514ce92f5a7785940dd0dd304fb29
Bekkalizer <markus.bekken@gmail.com>
2017-08-08 14:54:13 +0200

    Event creation fix
    Use correct logic when "Ask user to create new event when stage is
    complete" was combined with hiding the next program stage

--- components/dataentry/dataentry-controller.js
+++ components/dataentry/dataentry-controller.js
@@ -2049,23 +2049,7 @@ trackerCapture.controller('DataEntryController',
                     }
                     else {
                         if ($scope.currentStage.allowGenerateNextVisit) {
-                            if($scope.currentStage.repeatable){
-                                $scope.showCreateEvent($scope.currentStage, $scope.eventCreationActions.schedule);
-                            }
-                            else{
-                                var index = -1, stage = null;
-                                for(var i=0; i<$scope.programStages.length && index===-1; i++){
-                                    if($scope.currentStage.id === $scope.programStages[i].id){
-                                        index = i;
-                                        stage = $scope.programStages[i+1];
-                                    }
-                                }
-                                if(stage ){
-                                    if(!$scope.eventsByStage[stage.id] || $scope.eventsByStage[stage.id] && $scope.eventsByStage[stage.id].length === 0){
-                                        $scope.showCreateEvent(stage, $scope.eventCreationActions.schedule);
-                                    }
-                                }                                
-                            }
+                            $scope.showCreateEvent(null, $scope.eventCreationActions.schedule);
                         }
                     }
                 }
