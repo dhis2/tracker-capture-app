2bd85f76ae32630d5e20ab1691b7ad38512d3087
abyot <abyota@gmail.com>
2017-08-21 12:21:40 +0200

    fix to script path

--- scripts/app.js
+++ scripts/app.js
@@ -17,6 +17,7 @@ import '../scripts/leftbar-menu-controller.js';
 import '../scripts/report-types-controller.js';
 import '../scripts/display-mode-controller.js';
 import '../scripts/sticky.min.js';
+import '../scripts/ng-csv.js';
 import '../components/dashboard/dashboard-controller.js';
 import '../components/dashboard/dashboard-widgets-controller.js';
 import '../components/registration/registration-controller.js';
