fb01b9fbdb4a6c42559347293ac140537e62be73
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2017-08-28 15:34:15 +0200

    Added auto-fill date (#67)
    - In the case where a program stage is displayed in the registration phase the reportdate will auto-fill (with enrollment date) when the user tabs/selects the program stages report date field.

--- components/registration/registration-controller.js
+++ components/registration/registration-controller.js
@@ -698,4 +698,10 @@ trackerCapture.controller('RegistrationController',
             dateSetter($scope, null);
         }
     };
+
+    $scope.setDateOnFocus = function(currentValue, date) {
+        if(!currentValue) {
+            $scope.currentEvent.eventDate = date;
+        }
+    };
 });
