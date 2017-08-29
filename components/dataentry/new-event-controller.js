048eb7b502cb3dffcfc20821b16fbb83334d1287
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2017-08-29 09:00:35 +0200

    Fixed bug where sort order was not working as intended (#68)
    * Fixed bug where sort order was not working as intended
    
    - Fixed a bug where when adding an event or scheduling an event the system would sugest the last progam stage instead of the first which is the most likely program stage to be selected next.
    
    * Removed parts of if check that broke logic for setting correct next stage

--- components/dataentry/new-event-controller.js
+++ components/dataentry/new-event-controller.js
@@ -134,7 +134,7 @@ trackerCapture.controller('EventCreationController',
             for(var j = 0; j < availableStagesOrdered.length; j++){
                 var availableStage = availableStagesOrdered[j];
                 
-                if( !availableStage || !availableStage.id || lastStageForEvents || lastStageForEvents.id ){
+                if( !availableStage || !availableStage.id ){
                     break;
                 }
                 
@@ -155,7 +155,7 @@ trackerCapture.controller('EventCreationController',
             }
             
             if(angular.isUndefined(suggestedStage)){
-                suggestedStage = availableStagesOrdered[availableStagesOrdered.length - 1];
+                suggestedStage = availableStagesOrdered[0];
             }
         }
         
