ef97a6ff2af165c559681d9bbbae1363cec0325c
Bekkalizer <markus.bekken@gmail.com>
2017-10-25 10:49:45 +0200

    Stricter application of rule effects

--- components/dataentry/dataentry-controller.js
+++ components/dataentry/dataentry-controller.js
@@ -227,9 +227,14 @@ trackerCapture.controller('DataEntryController',
         //Establish which event was affected:
         var affectedEvent = $scope.currentEvent;
         if (!affectedEvent || !affectedEvent.event) {
+<<<<<<< HEAD
             //The data entry widget does not have an event selected. 
             //Therefore applying rule effects from registration instead.
             affectedEvent = 'registration';
+=======
+            //The data entry widget does not have an event selected.
+            return;
+>>>>>>> 568541d... Stricter application of rule effects
         }
         else if(event === 'registration' || event === 'dataEntryInit') {
            //The data entry widget is associated with an event, 
@@ -237,14 +242,13 @@ trackerCapture.controller('DataEntryController',
            return;
         }
 
-        //In most cases the updated effects apply to the current event. In case the affected event is not the current event, fetch the correct event to affect:
         if (event !== affectedEvent.event) {
-            angular.forEach($scope.allEventsSorted, function (searchedEvent) {
-                if (searchedEvent.event === event) {
-                    affectedEvent = searchedEvent;
-                }
-            });
+            //if the current event is not the same as the affected event, 
+            //the effecs should be disregarded in the current events controller instance.
+            $log.warn("Event " + event + " was not found in the current scope.");
+            return;
         }
+
 
         $scope.assignedFields[event] = [];
         $scope.hiddenSections[event] = [];
