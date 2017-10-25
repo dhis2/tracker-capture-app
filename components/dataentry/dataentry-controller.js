478132b0bdc4af05e16af5a021a5168977c3aaab
zepto5 <andre.hukkelaas@abaris.no>
2017-10-25 13:46:54 +0200

    Revert "Stricter application of rule effects"
    This reverts commit f34659986a9cac93313fefc7330b71d883a29558.

--- components/dataentry/dataentry-controller.js
+++ components/dataentry/dataentry-controller.js
@@ -227,8 +227,9 @@ trackerCapture.controller('DataEntryController',
         //Establish which event was affected:
         var affectedEvent = $scope.currentEvent;
         if (!affectedEvent || !affectedEvent.event) {
-            //The data entry widget does not have an event selected.
-            return;
+            //The data entry widget does not have an event selected. 
+            //Therefore applying rule effects from registration instead.
+            affectedEvent = 'registration';
         }
         else if(event === 'registration' || event === 'dataEntryInit') {
            //The data entry widget is associated with an event, 
@@ -236,13 +237,14 @@ trackerCapture.controller('DataEntryController',
            return;
         }
 
+        //In most cases the updated effects apply to the current event. In case the affected event is not the current event, fetch the correct event to affect:
         if (event !== affectedEvent.event) {
-            //if the current event is not the same as the affected event, 
-            //the effecs should be disregarded in the current events controller instance.
-            $log.warn("Event " + event + " was not found in the current scope.");
-            return;
+            angular.forEach($scope.allEventsSorted, function (searchedEvent) {
+                if (searchedEvent.event === event) {
+                    affectedEvent = searchedEvent;
+                }
+            });
         }
-
 
         $scope.assignedFields[event] = [];
         $scope.hiddenSections[event] = [];
