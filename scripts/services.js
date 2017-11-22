f1461e949985903210c8a120004e0aa42eaebec5
zepto5 <andre.hukkelaas@abaris.no>
2017-11-22 12:22:38 +0100

    store zero value fix

--- scripts/services.js
+++ scripts/services.js
@@ -1795,8 +1795,8 @@ var trackerCaptureServices = angular.module('trackerCaptureServices', ['ngResour
             };
 
         angular.forEach(programStage.programStageDataElements, function(prStDe){
-            if(dhis2Event[prStDe.dataElement.id]){                    
-                var value = CommonUtils.formatDataValue(dhis2Event.event, dhis2Event[prStDe.dataElement.id], prStDe.dataElement, optionSets, 'API');                    
+            if(dhis2Event[prStDe.dataElement.id] || dhis2Event[prStDe.dataElement.id] === 0){
+                var value = CommonUtils.formatDataValue(dhis2Event.event, dhis2Event[prStDe.dataElement.id], prStDe.dataElement, optionSets, 'API');
                 var val = {value: value, dataElement: prStDe.dataElement.id};
                 if(dhis2Event.providedElsewhere[prStDe.dataElement.id]){
                     val.providedElsewhere = dhis2Event.providedElsewhere[prStDe.dataElement.id];
