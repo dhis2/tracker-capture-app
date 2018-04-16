dd064d0e67f650420b9cdcd6c6c522af69f8fc78
Bekkalizer <markus.bekken@gmail.com>
2018-04-16 11:37:06 +0200

    Some cleanup and a bugfix for display text rules.

--- components/rulebound/rulebound-controller.js
+++ components/rulebound/rulebound-controller.js
@@ -50,70 +50,8 @@ trackerCapture.controller('RuleBoundController',
 
         //listen for updated rule effects
     $scope.$on('ruleeffectsupdated', function(event, args) {
-        $scope.data = RuleBoundFactory.getDisplayEffects($scope.data, args.event, $rootScope.ruleeffects, $scope.widgetTitle);
-    });
-    
-    var updateEffects = function(event)
-    {
-        var textInEffect = false;
-        var keyDataInEffect = false;
-        
-        if(event === 'registration') return;
-
-        //In case the 
-        if($scope.lastEventUpdated !== event) {
-            $scope.displayTextEffects = {};
-            $scope.displayKeyDataEffects = {};
-            $scope.lastEventUpdated = $rootScope.lastEventUpdated = event;
+        if( args.event !== 'registration' ) {
+            $scope.data = RuleBoundFactory.getDisplayEffects($scope.data, args.event, $rootScope.ruleeffects, $scope.widgetTitle);
         }
-        
-        if($rootScope.ruleeffects && $rootScope.ruleeffects[event]){
-            angular.forEach($rootScope.ruleeffects[event], function(effect) {
-                var g= 1;
-                var u = g+1;
-                if(effect.location === $scope.widgetTitle){
-                    //This effect is affecting the local widget
-                    
-                    //Round data to two decimals if it is a number:
-                    if(dhis2.validation.isNumber(effect.data)){
-                        effect.data = Math.round(effect.data*100)/100;
-                    }
-                    
-                    if(effect.action === "DISPLAYTEXT") {
-                        //this action is display text. Make sure the displaytext is
-                        //added to the local list of displayed texts
-                        if(!angular.isObject($scope.displayTextEffects[effect.id])){
-                            $scope.displayTextEffects[effect.id] = effect;
-                        }
-                        if(effect.ineffect)
-                        {
-                            textInEffect = true;
-                        }
-                    }
-                    else if(effect.action === "DISPLAYKEYVALUEPAIR") {                    
-                        //this action is display text. Make sure the displaytext is
-                        //added to the local list of displayed texts
-                        if(!angular.isObject($scope.displayTextEffects[effect.id])){
-                            $scope.displayKeyDataEffects[effect.id] = effect;
-                        }
-                        if(effect.ineffect)
-                        {
-                            keyDataInEffect = true;
-                        }
-                    }
-                    else if(effect.action === "ASSIGN") {
-                        //the dataentry control saves the variable and or dataelement
-                    }
-                    else {
-                        $log.warn("action: '" + effect.action + "' not supported by rulebound-controller.js");
-                    }
-                }
-            });
-        }
-        
-        
-        $scope.showKeyDataSection = keyDataInEffect;
-        $scope.showTextSection = textInEffect;   
-
-    } 
+    }); 
 });
