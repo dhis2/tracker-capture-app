bb814a77247f6782f19015e80e2291ff3b262bf0
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2018-08-31 15:04:41 +0200

    Fix for DHIS2-3605

--- components/dataentry/dataentry-controller.js
+++ components/dataentry/dataentry-controller.js
@@ -3202,6 +3202,11 @@ trackerCapture.controller('DataEntryController',
     $scope.hideEditAttributeCategoryOptions = function(){
         $scope.showAttributeCategoryOptions = !$scope.showAttributeCategoryOptions;
     };
+
+    $scope.calcTableWidth = function(){
+        var width = angular.element(document.getElementById('tabelContainer'))[0].clientWidth;
+        return width;
+    };
     
 })
 .controller('EventOptionsInTableController', function($scope, $translate){
