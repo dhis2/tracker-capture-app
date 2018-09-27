b8464310669fcb7a8369c5d5c3fede30718394d9
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2018-09-27 16:41:41 +0200

    Fix for deleting enrollments and TEI without cascading delete authority

--- components/dashboard/dashboard-controller.js
+++ components/dashboard/dashboard-controller.js
@@ -455,6 +455,10 @@ trackerCapture.controller('DashboardController',
         $scope.dataEntryMainMenuItemSelected = true;
     });
 
+    $scope.$on('ErollmentDeleted', function (args, data) {
+        $scope.allEnrollments = data.enrollments;
+    });
+
     $scope.$on('DataEntryMainMenuVisibilitySet', function (event, data) {
         if (data.visible) {
             //hide all widgets except visibleItems in data
@@ -586,11 +590,9 @@ trackerCapture.controller('DashboardController',
                             CurrentSelection.setTrackedEntityTypes(teis);
                         }
                     }
-                    
-                    NotificationService.showNotifcationDialog($translate.instant('success'), $scope.trackedEntityType.displayName + ' ' + $translate.instant('deleted'));                
-                    $scope.back();
                 }
-                
+                NotificationService.showNotifcationDialog($translate.instant('success'), $scope.trackedEntityType.displayName + ' ' + $translate.instant('deleted'));                
+                $scope.back();                
             });
         });
     };
