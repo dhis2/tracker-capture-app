f1af29425bb48eb932ea69784d22aaa6c43ede50
zepto5 <andre.hukkelaas@abaris.no>
2018-11-12 14:10:51 +0100

    cleanup filtering dataElements and trackedEntityAttributes

--- scripts/services.js
+++ scripts/services.js
@@ -449,12 +449,6 @@ var trackerCaptureServices = angular.module('trackerCaptureServices', ['ngResour
                                         prs.access = accesses.programStagesById[prs.id];
                                         accessiblePrs.push(prs);
                                     }
-                                    var accessibleDataElements = [];
-                                    angular.forEach(prs.programStageDataElements, function(de) {
-                                        if(de.access && de.access.read){
-                                            accessibleDataElements.push(de);
-                                        }
-                                    });
                                 });
                                 pr.programStages = accessiblePrs;
                                 programs.push(pr);
