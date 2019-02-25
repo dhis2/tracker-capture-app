b34d8e6f923c194ce6a7d52c1a4c4e36f55daf1a
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2019-02-25 09:20:31 +0100

    Use current program if no porgram is set in relationship

--- components/relationship/relationship-controller.js
+++ components/relationship/relationship-controller.js
@@ -170,8 +170,11 @@ trackerCapture.controller('RelationshipController',
                 var teiId = rel.to.trackedEntityInstance.trackedEntityInstance;
                 var relName = rel.relationshipName;
                 TEIService.get(teiId, $scope.optionSets, $scope.attributesById).then(function(tei){
-                    relationshipProgram = $scope.relationshipTypes.find(function(relType) { return relType.id === rel.relationshipType }).toConstraint.program;      
-                    var relative = {trackedEntityInstance: teiId, relName: relName, relId: rel.relationship, attributes: getRelativeAttributes(tei.attributes), relationshipProgramConstraint: relationshipProgram ? relationshipProgram : null};            
+                    relationshipProgram = $scope.relationshipTypes.find(function(relType) { return relType.id === rel.relationshipType }).toConstraint.program;
+                    if(!relationshipProgram && $scope.selectedProgram) {
+                        relationshipProgram = {id: $scope.selectedProgram.id};
+                    }
+                    var relative = {trackedEntityInstance: teiId, relName: relName, relId: rel.relationship, attributes: getRelativeAttributes(tei.attributes), relationshipProgramConstraint: relationshipProgram};            
                     $scope.relatedTeisTo.push(relative);
                 });
             } else if(rel.from && rel.from.trackedEntityInstance && rel.from.trackedEntityInstance.trackedEntityInstance !== $scope.selectedTei.trackedEntityInstance){  
@@ -179,7 +182,10 @@ trackerCapture.controller('RelationshipController',
                 var relName = rel.relationshipName;
                 TEIService.get(teiId, $scope.optionSets, $scope.attributesById).then(function(tei){
                     relationshipProgram = $scope.relationshipTypes.find(function(relType) { return relType.id === rel.relationshipType }).fromConstraint.program;
-                    var relative = {trackedEntityInstance: teiId, relName: relName, relId: rel.relationship, attributes: getRelativeAttributes(tei.attributes), relationshipProgramConstraint: relationshipProgram ? relationshipProgram : null};            
+                    if(!relationshipProgram && $scope.selectedProgram) {
+                        relationshipProgram = {id: $scope.selectedProgram.id};
+                    }
+                    var relative = {trackedEntityInstance: teiId, relName: relName, relId: rel.relationship, attributes: getRelativeAttributes(tei.attributes), relationshipProgramConstraint: relationshipProgram};            
                     $scope.relatedTeisFrom.push(relative);
                 });
             }
