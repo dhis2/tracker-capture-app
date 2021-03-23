687b65c74bc3e1be8a350f8d2b3151382ab3e338
Markus Bekken <markus.bekken@gmail.com>
2021-03-23 01:22:16 +0100

    feat: ordering of indicators

--- components/rulebound/rulebound-controller.js
+++ components/rulebound/rulebound-controller.js
@@ -25,8 +25,8 @@ trackerCapture.controller('RuleBoundController',
     $scope.lastEventUpdated = null;
     $scope.widgetTitleLabel = $translate.instant($scope.widgetTitle);
     
-    $scope.displayTextEffects = {};
-    $scope.displayKeyDataEffects = {};
+    $scope.displayTextEffects = [];
+    $scope.displayKeyDataEffects = [];
     
     var currentEventId = null;
 
@@ -52,16 +52,31 @@ trackerCapture.controller('RuleBoundController',
 
         //listen for updated rule effects
     $scope.$on('ruleeffectsupdated', function(event, args) {
-        if( args.event !== 'registration' ) {
-            $scope.data = RuleBoundFactory.getDisplayEffects($scope.data, args.event, $rootScope.ruleeffects, $scope.widgetTitle);
-        }
+        setOrderedData(RuleBoundFactory.getDisplayEffects($scope.data, args.event, $rootScope.ruleeffects, $scope.widgetTitle));
     });
 
     $scope.$on('dataEntryEventChanged', function(event,args){
         if(currentEventId !== args.event){
             currentEventId = args.event;
-            $scope.data = RuleBoundFactory.getDisplayEffects($scope.data,currentEventId, $rootScope.ruleeffects, $scope.widgetTitle);
+            setOrderedData(RuleBoundFactory.getDisplayEffects($scope.data,currentEventId, $rootScope.ruleeffects, $scope.widgetTitle));
         }
         
     });
+
+    var setOrderedData = function(data) {
+        $scope.displayTextEffects = [];
+        $scope.displayKeyDataEffects = [];
+
+        angular.forEach(Object.keys(data.displayKeyDataEffects), function(key){
+            if(data.displayKeyDataEffects[key].ineffect) {
+                $scope.displayKeyDataEffects.push({title: data.displayKeyDataEffects[key].content, value:data.displayKeyDataEffects[key].data});
+            }
+        });
+
+        angular.forEach(Object.keys(data.displayTextEffects), function(key){
+            if(data.displayTextEffects[key].ineffect) {
+                $scope.displayTextEffects.push({title: data.displayTextEffects[key].content, value:data.displayTextEffects[key].data});
+            }
+        });
+    }
 });
