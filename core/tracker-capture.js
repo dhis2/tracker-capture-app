e8d1c6e194524d697507a7e3360c63950d48342d
Markus Bekken <markus.bekken@gmail.com>
2021-04-15 20:58:57 +0200

    fix: download styles

--- core/tracker-capture.js
+++ core/tracker-capture.js
@@ -355,7 +355,7 @@ function getBatchPrograms( programs, batch )
         data: 'fields=id,displayFormName,version,displayEnrollmentDateLabel,enrollmentDateLabel,maxTeiCountToReturn,selectIncidentDatesInFuture,' +
         'incidentDateLabel,selectEnrollmentDatesInFuture,registration,favorite,useFirstStageDuringRegistration,displayName,' +
         'completeEventsExpiryDays,description,displayShortName,externalAccess,withoutRegistration,minAttributesRequiredToSearch,' + 
-        'displayFrontPageList,programType,accessLevel,displayIncidentDate,expiryDays,' +
+        'displayFrontPageList,programType,accessLevel,displayIncidentDate,expiryDays,style[*],' +
         'dataEntryForm[*],relatedProgram[id,displayName],relationshipType[id,displayName],featureType,trackedEntityType[id,displayName],categoryCombo[id,displayName,isDefault,categories[id,displayName,categoryOptions[id,displayName,organisationUnits[id]]]],userRoles[id,displayName],programStages[*,dataEntryForm[*],programStageSections[id,displayName,description,sortOrder,dataElements[id]],programStageDataElements[*,dataElement[*,optionSet[id]]]],programTrackedEntityAttributes[*,trackedEntityAttribute[id,unique,orgunitScope]],minAttributesRequiredToSearch,maxTeiCountToReturn&paging=false&filter=id:in:' + ids
     }).done( function( response ){
 
