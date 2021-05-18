bb78c421eb731dd936c94a6350c0950a9f1140c8
Sveinung Fjær <sveinung.fjaer@webstep.no>
2021-05-18 10:32:18 +0200

    Update names and comments

--- ks_patches/field_updates.js
+++ ks_patches/field_updates.js
@@ -6,7 +6,7 @@ export function conditionalAutofillBirthdateOnDnumberChange(tei, field) {
         return;
     }
 
-    // Do not override already set birth date
+    // Do not overwrite already set birth date
     if (tei[BIRTH_DATE_FIELD_CODE]) {
         return;
     }
@@ -22,15 +22,18 @@ function getValidDateOrUndefined(dnumber) {
     var day = dnumber.substr(0,2);
     var month = dnumber.substr(2,2);
     var year = dnumber.substr(4,2);
-    // Simple valid date test
-    if(day > 0 && day <= 31 && month > 0 && month <= 12 && year < 100 && year > -1) {
-        return day + '-' + month + '-' + guessCentury(year);
+    // Simple valid date test. Can give incorrect dates for months with less than 30 days.
+    // Should not be a problem for valid fnr, and is handled ok in application.
+    if( day > 0 && day <= 31 &&
+        month > 0 && month <= 12 &&
+        year < 100 && year > -1) {
+        return day + '-' + month + '-' + getFullYear(year);
     }
 
     return undefined;
 }
 
-function guessCentury(year) {
+function getFullYear(year) {
     var thisYear =  new Date().getFullYear() - 2000; // Gets two digit year of today
 
     // Will wrongly guess age of 100+ year olds as babies
