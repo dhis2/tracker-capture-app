f4a660ed6a18987fa7d568ca9c325d7e80b5da06
Sveinung Fjær <sveinung.fjaer@webstep.no>
2021-05-19 13:18:58 +0200

    Transfer helsestatus and testresult as well

--- components/registration/registration-controller.js
+++ components/registration/registration-controller.js
@@ -1,7 +1,9 @@
 /* global trackerCapture, angular */
 import { processRegistration } from './processRegistration';
 import {conditionalAutofillBirthdateOnDnumberChange} from "../../ks_patches/field_updates";
-import {transferNotesFromNaerkontaktToIndeksering} from "../../ks_patches/data_transfer";
+import {
+    transferDataFromNaerkontaktToIndeksering,
+} from "../../ks_patches/data_transfer";
 
 var trackerCapture = angular.module('trackerCapture');
 trackerCapture.controller('RegistrationController', 
@@ -545,7 +547,7 @@ trackerCapture.controller('RegistrationController',
                                     NotificationService.showNotifcationDialog($translate.instant("enrollment_error"), enrollmentResponse.message);
                                     return;
                                 }
-                                transferNotesFromNaerkontaktToIndeksering(enrollment, $scope.tei,  $scope.selectedEnrollment.program, EnrollmentService)
+                                transferDataFromNaerkontaktToIndeksering(enrollment, $scope.tei,  $scope.selectedEnrollment.program, EnrollmentService, DHIS2EventFactory);
                             }
                         });
                     }
--- ks_patches/data_transfer.js
+++ ks_patches/data_transfer.js
@@ -1,25 +1,56 @@
-import {NEARKONTAKT_PROGRAM_CODE, INDEKSERING_PROGRAM_CODE} from "../utils/constants";
+import {
+    NEARKONTAKT_PROGRAM_CODE,
+    INDEKSERING_PROGRAM_CODE,
+    NAERKONTAKT_TESTRESULT_PROGRAM_STAGE_CODE,
+    INDEKSERING_TESTRESULT_PROGRAM_STAGE_CODE,
+    NAERKONTAKT_HELSESTATUS_PROGRAM_STAGE_CODE, INDEKSERING_HELSESTATUS_PROGRAM_STAGE_CODE
+} from "../utils/constants";
 
-export function transferNotesFromNaerkontaktToIndeksering(enrollmentResponse, tei, program, enrollmentService){
-
-    // Transfer only for indeksregistrering og oppfølging
+export function transferDataFromNaerkontaktToIndeksering(enrollmentResponse, tei, program, enrollmentService, eventFactory) {
+    // Transfer only to indeksregistrering og oppfølging
     if(program !== INDEKSERING_PROGRAM_CODE) {
         return;
     }
 
     if(tei.enrollments) {
-        tei.enrollments.map( enrollment => {
-            // Transfer only notes from Nærkontaktregistrering
-            if( enrollment.program == NEARKONTAKT_PROGRAM_CODE) {
-                var e = angular.copy(enrollmentResponse);
-                delete e.enrollmentDate; // Date on wrong format. Not needed
-                delete e.incidentDate; // Date on wrong format. Not needed
-                e.notes = enrollment.notes.map(note => {
-                    delete note.note; // Cannot reuse note ID
-                    return note;
-                })
-                enrollmentService.updateForNote(e);
+        tei.enrollments.forEach( enrollment => {
+            // Transfer only data from Nærkontaktregistrering
+            if (enrollment.program == NEARKONTAKT_PROGRAM_CODE) {
+                transferEvents(enrollment.events, enrollmentResponse.enrollment, NAERKONTAKT_TESTRESULT_PROGRAM_STAGE_CODE, INDEKSERING_PROGRAM_CODE, INDEKSERING_TESTRESULT_PROGRAM_STAGE_CODE, eventFactory);
+                transferEvents(enrollment.events, enrollmentResponse.enrollment, NAERKONTAKT_HELSESTATUS_PROGRAM_STAGE_CODE, INDEKSERING_PROGRAM_CODE, INDEKSERING_HELSESTATUS_PROGRAM_STAGE_CODE, eventFactory);
+                transferNotes(enrollment.notes, enrollmentResponse, enrollmentService);
             }
         });
     }
 }
+
+function transferEvents(events, enrollmentId, fromProgramStage, toProgram, toProgramStage, eventFactory){
+    events.forEach( event => {
+        if(event.programStage == fromProgramStage) {
+            var newEvent = angular.copy(event);
+            newEvent.program = toProgram;
+            newEvent.programStage = toProgramStage;
+            newEvent.enrollment = enrollmentId;
+            if(newEvent.notes) {
+                newEvent.notes = newEvent.notes.map(note => {
+                    delete note.note; // Cannot reuse note ID
+                    return note;
+                });
+            }
+            delete newEvent.event; // Cannot reuse event ID
+            eventFactory.create(newEvent);
+        }
+    });
+}
+
+function transferNotes(notes, enrollmentResponse, enrollmentService) {
+    var enrollmentUpdate = angular.copy(enrollmentResponse);
+    delete enrollmentUpdate.enrollmentDate; // Date on wrong format. Not needed
+    delete enrollmentUpdate.incidentDate; // Date on wrong format. Not needed
+    enrollmentUpdate.notes = notes.map(note => {
+        delete note.note; // Cannot reuse note ID
+        return note;
+    });
+    enrollmentService.updateForNote(enrollmentUpdate);
+}
+
