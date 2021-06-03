009360dd2e0f81bf482be0796de83362e30f2bb7
Markus Bekken <markus.bekken@gmail.com>
2021-06-03 23:48:22 +0200

    fix: use first contact date that falls within cluster dates

--- components/relationship/relationship-controller.js
+++ components/relationship/relationship-controller.js
@@ -205,12 +205,15 @@ trackerCapture.controller('RelationshipController',
             TEIService.getWithProgramData(relative.trackedEntityInstance, 'DM9n1bUw8W8', $scope.optionSets, $scope.attributesById).then(function(teiIndex){
                 angular.forEach(teiIndex.enrollments,function(enrollment) {
                     if(enrollment.program == 'DM9n1bUw8W8') {
-                        var contactDateMoment = moment(DateUtils.formatFromUserToApi(enrollment.enrollmentDate));
+                        var contactDateMoment;
 
                         angular.forEach(enrollment.events, function(event){
-                            if((!endDate || moment(event.eventDate).isBefore(endDate)) && moment(event.eventDate).isAfter(startDate) && event.programStage == 'sAV9jAajr8x' ) {
+                            if((!endDate || moment(event.eventDate).isBefore(endDate)) && (moment(event.eventDate).isAfter(startDate) || moment(event.eventDate).isSame(startDate)) && event.programStage == 'sAV9jAajr8x' ) {
                                 //this is the followup event in the contact program: event date is contact time.
-                                contactDateMoment = moment(event.eventDate);
+                                if (!contactDateMoment || moment(event.eventDate).isBefore(contactDateMoment)) {
+                                    //only update the contact date if it is older than the previous one, we want the first contact date that is relevant
+                                    contactDateMoment = moment(event.eventDate);
+                                }
                             }
                         });
 
