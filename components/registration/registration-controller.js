9ffc4282e850459954a8172c616e46153a1b56b1
Sveinung Fjær <sveinung.fjaer@webstep.no>
2021-11-26 12:20:11 +0100

    Fix double scope

--- components/registration/registration-controller.js
+++ components/registration/registration-controller.js
@@ -1522,7 +1522,7 @@ trackerCapture.controller('RegistrationController',
                     attributesById: $scope.attributesById,
                     selectedTei: $scope.selectedTei,
                     attributes: $scope.attributes,
-                    searchFnr: $scope.$scope.fNrOrEquivalentGetterSetter()
+                    searchFnr: $scope.fNrOrEquivalentGetterSetter()
                 }
 
                 return $modal.open({
