563a838e28de9103a568849d71c60de11f5bc395
Sveinung Fjær <34539750+SveinungFjaer@users.noreply.github.com>
2022-02-07 19:24:30 +0100

    feat: Default page size to 50 (#1365)

--- ks_patches/override_params.js
+++ ks_patches/override_params.js
@@ -2,10 +2,10 @@ import {INDEKSERING_PROGRAM_ID, NAERKONTAKT_PROGRAM_ID} from "../utils/constants
 
 export function customPageSizeForProgram(program) {
     if(program === INDEKSERING_PROGRAM_ID) {
-        return 150;
+        return 50;
     }
     if(program === NAERKONTAKT_PROGRAM_ID) {
-        return 150;
+        return 50;
     }
     return undefined;
 }
