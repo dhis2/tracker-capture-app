e0655d65cfed0718544a736862d0a455a0a4ec13
Tony Valle <tony@devotta.no>
2022-12-22 13:47:30 +0100

    fix: add missing comma

--- d2-tracker/dhis2.angular.services.js
+++ d2-tracker/dhis2.angular.services.js
@@ -2798,7 +2798,7 @@ var d2Services = angular.module('d2Services', ['ngResource'])
                 }
                 return output;
             },
-        }
+        },
         "d2:count": {
             parameters: 1,
             execute: function(parameters, variablesHash) {
