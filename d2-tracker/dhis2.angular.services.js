1093facf810a3cccaac82fcd85cc2af9f6729ab1
Tony Valle <tony@devotta.no>
2022-12-22 15:15:42 +0100

    fix: [DHIS2-14352] remove merge conflict artifact

--- d2-tracker/dhis2.angular.services.js
+++ d2-tracker/dhis2.angular.services.js
@@ -2633,7 +2633,7 @@ var d2Services = angular.module('d2Services', ['ngResource'])
                     output = 0;
                 }
                 return output;
-            },sssssssssssssssssssssssssssssssssssssssssssss
+            },
         },
         "d2:count": {
             parameters: 1,
