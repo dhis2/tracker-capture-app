2d456b195d36078fa9cb346b7b8a1fde1011a497
Tony Valle <tony@devotta.no>
2023-05-08 14:56:18 +0200

    fix: support newline in program rule expressions

--- d2-tracker/dhis2.angular.services.js
+++ d2-tracker/dhis2.angular.services.js
@@ -2957,7 +2957,8 @@ var d2Services = angular.module('d2Services', ['ngResource'])
         "d2:length": {
             parameters: 1,
             execute: function(parameters) {
-                return String(parameters[0]).length;
+                const text = typeof parameters[0] === 'string' ? parameters[0] : String(parameters[0]);
+                return text.replace(/\n/g,'').length;
             },
         },
         "d2:condition": {
@@ -3009,7 +3010,8 @@ var d2Services = angular.module('d2Services', ['ngResource'])
      * @returns {*}
      */
     function evaluate(code) {
-        const func = new Function(`"use strict";return ${code}`);
+        const codeWithNewline = code.replace(/\n/g, '\\n');
+        const func = new Function(`"use strict";return ${codeWithNewline}`);
         return func();
     }
 
