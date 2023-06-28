1643851985ef0b13ecb46dcbb14faecfa9a1e97f
Tony Valle <tony@devotta.no>
2023-06-28 21:40:02 +0200

    fix: use `displayFormName` instead of `formName`

--- scripts/services.js
+++ scripts/services.js
@@ -2144,7 +2144,7 @@ var trackerCaptureServices = angular.module('trackerCaptureServices', ['ngResour
             angular.forEach(attributes, function(attr){
                 if(attr.displayInListNoProgram){
                     gridColumnIndex++;
-                    var gridColumn = {id: attr.id, displayName: attr.displayName, formName: attr.formName, show: false, valueType: attr.valueType};
+                    var gridColumn = {id: attr.id, displayName: attr.displayName, displayFormName: attr.displayFormName, show: false, valueType: attr.valueType};
                     setShowGridColumn(gridColumn,gridColumnIndex, config, savedGridColumnsKeyMap);
                     gridColumns.push(gridColumn);
                 }
